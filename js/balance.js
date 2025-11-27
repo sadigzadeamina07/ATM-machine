4a87b899fad9aa289393a11a58abb7e1d4c8a831
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2025-11-27 04:52:30 +0400

    our out button is working rn

--- js/balance.js
+++ js/balance.js
@@ -1,25 +1,30 @@
 let balansshow = document.getElementById('balansshow');
 let balansInput = document.getElementById('balans');
 let balans = 0;
-function updateBalance() {
-    balansshow.innerHTML = balans.toFixed(2) + "$";
-}
+
 function balanceout(kod) {
     let amount = Number(balansInput.value);
+
+    if (!amount || amount <= 0) {
+        alert("Məbləğ düzgün deyil!");
+        return; 
+    }
     switch (kod) {
-        case 0:
-            updateBalance();
-            break;
-        case 1:
-            if (!amount || amount <= 0) {
-                alert("Məbləğ düzgün deyil!");
+        case 0: 
+            if (amount > balans) {
+                alert("Balans kifayət deyil!");
                 return;
             }
-            balans += amount;
-            updateBalance();
-            balansInput.value = "";
+                 balansshow.innerHTML = amount.toFixed(2) + "$";
             break;
+
+        case 1: 
+            balans += amount;
+                balansshow.innerHTML = balans.toFixed(2) + "$";
+            break;
+
         default:
             alert("Yanlış əməliyyat");
     }
+    balansInput.value = "";    
 }
